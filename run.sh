60ef337aef7279af135ba6ebde3bfdc830f86323
Clément Boudereau <cboudereau@gmail.com>
2024-09-19 10:39:43 +0200

    fix run script demo

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 docker build -t git-turf:latest .
-git clone <YourAccount>/helloworld.git
+git clone <YourAccount>/<YourRepo>.git
+cd <YourRepo>
 git config user.email "<YourEmail>"
 git config user.name "<YourName>"
-docker run --rm -v $(pwd):/workspace -v ~/.ssh:/home/.ssh:ro git-turf "Hello, world"
-#   #      ## ##                            ##    #
-#   #       #  #                             #    #
-#   #  ##   #  #  ##        # # #  ##  # ##  #  ###
-##### #  #  #  # #  #       # # # #  # ##    # #  #
-#   # ####  #  # #  # ##    # # # #  # #     # #  #
-#   # #     #  # #  #  #     # #  #  # #     # #  #
-#   #  ##   #  #  ##  #      # #   ##  #     #  ###
+# dry-run
+docker run --rm -v $(pwd):/workspace git-turf --date 2022-01-01 --dry-run "Hello, world"
+docker run --rm -v $(pwd):/workspace git-turf --date 2022-01-09 --dry-run "It's a trap"
+
+# count commits
+git rev-list HEAD --count
