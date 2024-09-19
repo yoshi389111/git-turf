7c6490de059a989c848f450ddea72dbbf3eb0f3c
Clément Boudereau <cboudereau@gmail.com>
2024-09-19 11:16:12 +0200

    clean

--- run.sh
+++ run.sh
@@ -9,5 +9,8 @@ git config user.name "<YourName>"
 docker run --rm -v $(pwd):/workspace git-turf --date 2022-01-01 --dry-run "Hello, world"
 docker run --rm -v $(pwd):/workspace git-turf --date 2022-01-09 --dry-run "It's a trap"
 
+# run
+docker run --rm -v $(pwd):/workspace git-turf --date 2022-01-09 "It's a trap"
+
 # count commits
 git rev-list HEAD --count
